ee71eba5005b755e965587734c0410555a0d6ede
Calvin Rodo <calvinv@gmail.com>
2013-03-06 17:33:40 -0500

    Working on adding client side aws uploading.\.

--- express_settings.js
+++ express_settings.js
@@ -4,5 +4,6 @@ exports.Config = {
   AdminUsername: process.env.WedXpressUName || 'username',
   AdminPassword: process.env.WedXpressPWord || 'password',
   MongoDbConnection: process.env.MongoConnection || 'mongodb://localhost:27017/db',
-  HashIDSalt: process.env.HashIDSalt || 'Please replace with a salt'
+  AWSAccessKeyID: process.env.AWSPublicAccessKey,
+  AWSAccessKeySecret: process.env.AWSSecretAccessKey
 };
