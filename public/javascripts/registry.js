78d77d37d3ef900a954b22920082e6fbd60a4bdd
Calvin Rodo <calvinv@gmail.com>
2013-03-31 20:54:46 -0400

    Getting masonry to work

--- public/javascripts/registry.js
+++ public/javascripts/registry.js
@@ -1,12 +1,6 @@
 $(function () {
-  $(".item").hover(function () {
-    $(this).siblings().stop().fadeTo("fast", 0.5);
-  }, function () {
-    $(this).siblings().stop().fadeTo("fast", 1);
-  });
   $('#thumbnails').masonry({
-    itemSelector: '.item',
-    columnWidth: $('.span3').width()
+    itemSelector: '.item'
   });
 //  $('#RegistryContents').equalize();
 //  $('#RegistryContents').equalize('width');
