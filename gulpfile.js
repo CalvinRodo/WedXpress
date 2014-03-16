26be58b89cbbc8e1406235604ed38d7a81378ac3
Calvin Rodo <calvinv@gmail.com>
2014-03-16 18:47:12 -0400

    converting to angular

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,47 @@ var gulp = require('gulp');
 var bower = require('gulp-bower');
 var clean = require('gulp-clean');
 var less = require('gulp-less');
+var gutil = require('gulp-util');
+var livereload = require('gulp-livereload');
+var jade = require('gulp-jade');
 
-gulp.task('default', ['clean', 'bower', 'less'], function(){
+var EXPRESS_PORT = 4000;
+var EXPRESS_ROOT = __dirname;
+var LIVERELOAD_PORT = 35729;
+
+function startExpress() {
+ 
+  var express = require('express');
+  var app = express();
+  app.use(require('connect-livereload')());
+  app.use(express.static(EXPRESS_ROOT));
+  app.listen(EXPRESS_PORT);
+  gutil.log(EXPRESS_ROOT + ' served up on ' + EXPRESS_PORT);
+}
+
+var lr; 
+function startLivereload() {
+ 
+  lr = require('tiny-lr')();
+  lr.listen(LIVERELOAD_PORT);
+}
+
+gulp.task('default', ['clean', 'bower', 'less', 'watch'], function(){
+});
+
+gulp.task('jade', function(){
+	gulp.src('views/*.jade')
+		.pipe(jade())
+		.pipe(gulp.dest('jadeout'));
+});
+
+gulp.task('server', function(){
+	startExpress();
+	startLivereload();
 });
 
 gulp.task('clean', function(){
-	gulp.src(['public/styles/*', 'public/scripts/libs/*'])
+	gulp.src(['public/styles/*', 'public/scripts/libs/*'], { read: false })
 		.pipe(clean());
 });
 
@@ -19,6 +54,11 @@ gulp.task('bower', function(){
 gulp.task('less', function(){
 	gulp.src('src/styles/*.less')
 	  .pipe(less())
-	  .pipe(gulp.dest('public/styles/'));
+	  .pipe(gulp.dest('public/styles/'))
+	  .pipe(livereload());
+});
+
+gulp.task('watch', ['server'], function(){
+	gulp.watch('src/styles/*.less',['less']);
 });
 
